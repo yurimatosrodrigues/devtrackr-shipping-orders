849a1e16a120e018069e7bba6b0b0b58ed21163e
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2025-08-11 17:10:08 -0300

    feat: add MongoDB to project

---
+++ DevTrackR.ShippingOrders.Infrastructure/MongoDbOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace DevTrackR.ShippingOrders.Infrastructure
+{
+    public class MongoDbOptions
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
